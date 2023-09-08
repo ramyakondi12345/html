ad4015789a7a0a9ed2122653e79204c3ce58a87d
ramyakondi12345 <125718223+ramyakondi12345@users.noreply.github.com>
2023-09-08 11:39:30 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,51 +1,89 @@
-const smallCups=document.querySelectorAll('.cup-small')
-const liters=document.getElementById('liters')
-const percentage=document.getElementById('percentage')
-const remained=document.getElementById('remained')
+// Questions that will be asked
+const Questions = [{
+	q: "What is capital of India?",
+	a: [{ text: "Gandhinagar", isCorrect: false },
+	{ text: "Surat", isCorrect: false },
+	{ text: "Delhi", isCorrect: true },
+	{ text: "Mumbai", isCorrect: false }
+	]
 
-updateBigCup()
+},
+{
+	q: "What is the capital of Thailand?",
+	a: [{ text: "Lampang", isCorrect: false, isSelected: false },
+	{ text: "Phuket", isCorrect: false },
+	{ text: "Ayutthaya", isCorrect: false },
+	{ text: "Bangkok", isCorrect: true }
+	]
 
-smallCups.forEach((cup,idx)=>{
-    cup.addEventListener('click',() => {
-     highlightCups(idx)
-    })
-})
-  
-function highlightCups(idx){
-    if(idx===7 && smallCups[idx].classList.contains("full")) idx--;
-    else if(smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')){
-        idx--
-    }
-    smallCups.forEach((cup,idx2)=>{
-        if(idx2 <= idx){
-            cup.classList.add('full')
-        }else{
-            cup.classList.remove('full')
-        }
-        
-    })
-    updateBigCup()
+},
+{
+	q: "What is the capital of Gujarat",
+	a: [{ text: "Surat", isCorrect: false },
+	{ text: "Vadodara", isCorrect: false },
+	{ text: "Gandhinagar", isCorrect: true },
+	{ text: "Rajkot", isCorrect: false }
+	]
+
 }
-function updateBigCup(){
-    const fullCups =document.querySelectorAll('.cup-small.full').length
-    const totalCups = smallCups.length
 
-    if(fullCups===0){
-        percentage.style.visibility='hidden'
-        percentage.style.height=0
-    }else{
-        percentage.style.visibility='visible'
-        percentage.style.height = `${fullCups / totalCups * 330}px`
-        percentage.innerText = `${fullCups / totalCups * 100}%`
-    
-    }
-    if(fullCups === totalCups){
-        remained.style.visibility='hidden'
-        remained.style.height=0
-    }
-    else{
-        remained.style.visibility='visible'
-        liters.innerText = `${2 - (250 * fullCups / 1000)}L`
+]
 
-    }
+let currQuestion = 0
+let score = 0
+
+function loadQues() {
+	const question = document.getElementById("ques")
+	const opt = document.getElementById("opt")
+
+	question.textContent = Questions[currQuestion].q;
+	opt.innerHTML = ""
+
+	for (let i = 0; i < Questions[currQuestion].a.length; i++) {
+		const choicesdiv = document.createElement("div");
+		const choice = document.createElement("input");
+		const choiceLabel = document.createElement("label");
+
+		choice.type = "radio";
+		choice.name = "answer";
+		choice.value = i;
+
+		choiceLabel.textContent = Questions[currQuestion].a[i].text;
+
+		choicesdiv.appendChild(choice);
+		choicesdiv.appendChild(choiceLabel);
+		opt.appendChild(choicesdiv);
+	}
+}
+
+loadQues();
+
+function loadScore() {
+	const totalScore = document.getElementById("score")
+	totalScore.textContent = `You scored ${score} out of ${Questions.length}`
+}
+
+
+function nextQuestion() {
+	if (currQuestion < Questions.length - 1) {
+		currQuestion++;
+		loadQues();
+	} else {
+		document.getElementById("opt").remove()
+		document.getElementById("ques").remove()
+		document.getElementById("btn").remove()
+		loadScore();
+	}
+}
+
+function checkAns() {
+	const selectedAns = parseInt(document.querySelector('input[name="answer"]:checked').value);
+
+	if (Questions[currQuestion].a[selectedAns].isCorrect) {
+		score++;
+		console.log("Correct")
+		nextQuestion();
+	} else {
+		nextQuestion();
+	}
 }
